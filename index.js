746636e57b26088545bc3a6643c9512966abfc02
anvesh <anveshpalli1304@gmail.com>
2024-07-24 06:51:25 +0000

    output

---
+++ index.js
@@ -0,0 +1,12 @@
+const element = (
+  //   Write your code here.
+  <div className='main-class'>
+    <h1>super over </h1>
+    <div>
+      <img src='https://assets.ccbp.in/frontend/react-js/rcb-img.png' />
+      <img src='https://assets.ccbp.in/frontend/react-js/csk-img.png' />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
